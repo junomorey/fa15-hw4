c8f65b68e53e4207056883160695ab714cb512b5
Juno Student <j.morey@berkeley.edu>
2015-10-09 14:10:35 -0700

    75% done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   get "users/new", to: "users#new"
   get "users/:id", to: "users#show", as: :user
   post "users", to: "users#create"
+
+  root 'dogs#index'
+  get "dogs/:id", to: "dogs#show", as :dog
+  get "dogs/new", to: "dogs#new"
+  post "dogs", to: "dogs#create"
+
 end
